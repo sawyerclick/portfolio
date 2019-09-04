61c97ab3b10a5856ef0932280b61409c58fa1f71
SawyerClick <src2188@columbia.edu>
2019-09-04 19:47:38 -0400

    + nav bar

--- js/main.js
+++ js/main.js
@@ -1,6 +1,25 @@
-$(".btn").click(function(e){
-  e.preventDefault();
-  var category = $(this).data("category")
-  $('.column').hide();
-  $('.column.'+category).fadeIn();
-})
+/* Smooth Scrolling
+* ------------------------------------------------------ */
+   var clSmoothScroll = function() {
+        
+    $('.smoothscroll').on('click', function (e) {
+        var target = this.hash,
+        $target    = $(target);
+        
+            e.preventDefault();
+            e.stopPropagation();
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top+94
+        }, cfg.scrollDuration, 'swing').promise().done(function () {
+
+            // check if menu is open
+            if ($('body').hasClass('menu-is-open')) {
+                $('.header-menu-toggle').trigger('click');
+            }
+
+            window.location.hash = target;
+        });
+    });
+
+};
