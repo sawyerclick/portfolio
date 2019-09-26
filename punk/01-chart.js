081a3b58d5214e0273321c3757a0978c8a158e15
SawyerClick <src2188@columbia.edu>
2019-09-25 20:10:17 -0400

    pause button, filters, restructuring

--- punk/01-chart.js
+++ punk/01-chart.js
@@ -1,16 +1,16 @@
 ;(function() {
+  // var audio = d => song_preview)
+
   // Build your SVG here
   // using all of that cut-and-paste magic
-  var margin = { top: 50, right: 50, bottom: 50, left: 50 }
+  var margin = { top: 150, right: 50, bottom: 50, left: 105 }
   var padding = { top: '25%', bottom: '25%' }
   var width = 750 - margin.left - margin.right
-  var height = 500 - margin.top - margin.bottom
+  var height = 250 - margin.top - margin.bottom
 
   var svg = d3
     .select('#chart1')
     .append('svg')
-    // .style('margin-left', 0)
-    // .style('margin-right', 0)
     .attr('width', width + margin.left + margin.right)
     .attr('height', height + margin.top + margin.bottom)
     .append('g')
@@ -28,48 +28,28 @@
   var radiusScale = d3
     .scaleSqrt()
     .domain([0, 100])
-    .range([1, 5])
+    .range([2, 7])
 
-  var keys = ['Energy', 'Danceability', 'Valence']
+  var keys = ['Valence', 'Danceability', 'Energy']
 
   var color = d3
     .scaleOrdinal()
     .domain(keys)
-    .range(['#C7493A', '#5CDB95', '#dbc269'])
+    .range(['#dbc269', '#5CDB95', '#C7493A'])
 
   // Add one dot in the legend for each name.
   svg
-    .selectAll('mydots')
-    .data(keys)
-    .enter()
-    .append('circle')
-    .attr('cx', function(d, i) {
-      return 0 + i * 250
-    })
-    .attr('cy', 0) // 100 is where the first dot appears. 25 is the distance between dots
-    .attr('r', 7)
-    .style('fill', function(d) {
-      return color(d)
-    })
-
-  // Add one dot in the legend for each name.
-  svg
-    .selectAll('mylabels')
+    .selectAll('labels')
     .data(keys)
     .enter()
     .append('text')
-    .attr('x', function(d, i) {
-      return 0 + i * 260
+    .text(d => d)
+    .attr('x', -20)
+    .attr('y', function(d, i) {
+      return height - 45 - i * 50
     })
-    .attr('y', 0)
-    .style('fill', function(d) {
-      return color(d)
-    })
-    .text(function(d) {
-      return d
-    })
-    .attr('text-anchor', 'left')
-    .style('alignment-baseline', 'middle')
+    .style('fill', d => color(d))
+    .style('text-anchor', 'end')
 
   d3.csv('spotify_punk_playlists.csv')
     .then(ready)
@@ -78,13 +58,49 @@
     })
 
   function ready(datapoints) {
-    // catching variables for scale domains
-    // var loudness = datapoints.map(function(d) {
-    //   return d.loudness
-    // })
-    // xPositionScale.domain(loudness)
+    let audio = null
+    
+    d3.select('#all').on('click', function() {
+      console.log('clicked')
+      svg.selectAll('circle').attr('fill-opacity', 0)
+      svg
+        .selectAll('circle')
+        .transition()
+        .duration(1000)
+        .ease(d3.easeQuad)
+        .attr('fill-opacity', 0.3)
+    })
 
-    // Add and style your marks here
+    d3.select('i')
+      .attr('class', 'fa fa-pause')
+      .attr('x', 1500)
+      .on('click', d => {
+        if (audio) {
+          audio.pause()
+        }
+      })
+
+    d3.select('#explicit').on('click', function() {
+      console.log('clicked')
+      svg.selectAll('circle').attr('fill-opacity', 0)
+      svg
+        .selectAll('.true')
+        .transition()
+        .duration(1000)
+        .ease(d3.easeQuad)
+        .attr('fill-opacity', 0.3)
+    })
+
+    d3.select('#notExplicit').on('click', function() {
+      console.log('clicked')
+      svg.selectAll('circle').attr('fill-opacity', 0)
+      svg
+        .selectAll('.false')
+        .transition()
+        .duration(1000)
+        .ease(d3.easeQuad)
+        .attr('fill-opacity', 0.3)
+    })
 
     // energy
     svg
@@ -92,31 +108,44 @@
       .data(datapoints)
       .enter()
       .append('circle')
-      .attr('cy', 200)
+      .attr('cy', height - 150)
       .attr('cx', d => xPositionScale(d.energy))
       .attr('fill', '#C7493A')
       .attr('fill-opacity', 0.3)
-      .attr('r', 0)
-      .transition()
-      .duration(1000)
-      .ease(d3.easeQuad)
       .attr('r', d => radiusScale(d.song_popularity))
+      .attr('class', d => d.song_explicit.toLowerCase())
+      .on('mouseover', d => {
+        console.log(d.song_name, d.artist_name, d.playlist_name)
+      })
+      .on('click', d => {
+        if (audio) {
+          audio.pause()
+        }
+        audio = new Audio(d.song_preview)
+        return audio.play()
+      })
 
-    // danceability
     svg
       .selectAll('svg')
       .data(datapoints)
       .enter()
       .append('circle')
-      .attr('cy', 250)
+      .attr('cy', height - 100)
       .attr('cx', d => xPositionScale(d.danceability))
       .attr('fill', '#5CDB95')
-      .attr('r', 0)
       .attr('fill-opacity', 0.3)
-      .transition()
-      .duration(1000)
-      .ease(d3.easeQuad)
       .attr('r', d => radiusScale(d.song_popularity))
+      .attr('class', d => d.song_explicit.toLowerCase())
+      .on('mouseover', d => {
+        console.log(d.song_name, d.artist_name, d.playlist_name)
+      })
+      .on('click', d => {
+        if (audio) {
+          audio.pause()
+        }
+        audio = new Audio(d.song_preview)
+        return audio.play()
+      })
 
     // danceability
     svg
@@ -124,15 +153,22 @@
       .data(datapoints)
       .enter()
       .append('circle')
-      .attr('cy', 150)
+      .attr('cy', height - 50)
       .attr('cx', d => xPositionScale(d.valence))
       .attr('fill', '#dbc269')
-      .attr('r', 0)
       .attr('fill-opacity', 0.3)
-      .transition()
-      .duration(1000)
-      .ease(d3.easeQuad)
       .attr('r', d => radiusScale(d.song_popularity))
+      .attr('class', d => d.song_explicit.toLowerCase())
+      .on('mouseover', d => {
+        console.log(d.song_name, d.artist_name, d.playlist_name)
+      })
+      .on('click', d => {
+        if (audio) {
+          audio.pause()
+        }
+        audio = new Audio(d.song_preview)
+        return audio.play()
+      })
 
     var xAxis = d3.axisBottom(xPositionScale)
     svg
