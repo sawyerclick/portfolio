49186dce27f99b9b651d8718af1da5663b47f554
SawyerClick <src2188@columbia.edu>
2020-02-18 17:33:48 -0500

    immigration court
    added immigration court + fixed responsive

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,21 @@
 ;(function() {
-    d3.select('.fa-angellist')
+  d3.select('.fa-angellist')
     .on('mouseover', function() {
-        d3.select('#about')
+      d3.select('#about')
         .style('visibility', 'visible')
         .style('height', 'auto')
     })
     .on('mouseout', function() {
-        d3.select('#about')
+      d3.select('#about')
         .style('visibility', 'collapse')
         .style('height', 0)
     })
-  })()
-  
+
+  d3.select('#nbc').on('mouseover', function() {
+    d3.select('#nbc-clips').style('display', 'inline-block')
+  })
+
+  d3.select('#nbc-close').on('click', function() {
+    d3.select('#nbc-clips').style('display', 'none')
+  })
+})()
