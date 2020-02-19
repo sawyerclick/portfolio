94dcec83d6bb4b6cf0b39f530461b6cc755e6738
SawyerClick <src2188@columbia.edu>
2020-02-19 16:39:29 -0500

    color coded

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,11 +11,22 @@
         .style('height', 0)
     })
 
-  d3.select('#nbc').on('mouseover', function() {
-    d3.select('#nbc-clips').style('display', 'inline-block')
-  })
+  // d3.select('#nbc').on('mouseover', function() {
+  //   d3.select('#nbc-clips').style('display', 'inline-block')
+  // })
 
-  d3.select('#nbc-close').on('click', function() {
-    d3.select('#nbc-clips').style('display', 'none')
-  })
+  // d3.select('#nbc-close').on('click', function() {
+  //   d3.select('#nbc-clips').style('display', 'none')
+  // // })
+
+  // d3.select('#nbcProj').on('mouseover', function() {
+  //   d3.selectAll('.column').style('visibility', 'collapse')
+  // })
+
+  // d3.select('#sideProj').on('mouseover', function() {
+  //   console.log('hi')
+  //   d3.selectAll('.nbc')
+  //     .style('visibility', 'collapse')
+  //     .style('height', 0)
+  // })
 })()
