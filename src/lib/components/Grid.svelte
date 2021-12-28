e417e1d1f71d12f778c5e6290991b3a1e1197f01
Sawyer Click <src2188@columbia.edu>
2021-12-28 00:48:33 -0600

    padding on grid to match carousel

--- src/lib/components/Grid.svelte
+++ src/lib/components/Grid.svelte
@@ -6,7 +6,7 @@
 
 <ul
 	id="graphics"
-	class="w-full mx-auto grid px-6 gap-4 lg:gap-6 place-items-start grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4"
+	class="w-full mx-auto grid px-6 py-5 gap-4 lg:gap-6 place-items-start grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4"
 >
 	{#each items as project (project.title)}
 		<li class="project relative w-full h-full" animate:flip>
