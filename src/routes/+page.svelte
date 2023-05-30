2ffaf1bfbbf865f1b2f87d71f821b0600b6d762b
Sawyer Click <src2188@columbia.edu>
2023-05-30 00:29:32 -0500

    add works + open source sections

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -107,6 +107,47 @@
 
 <Separator class="mx-8" />
 
+<section id="recent" class="mx-8 my-8">
+	<h2 class="font-gothic text-6xl mb-4">Recent works</h2>
+	<ol>
+		{#each data.projects.recent as project (project.title)}
+			<li class="project snap-center shrink-0 w-96 sm:snap-start">
+				<article>
+					<h3 class="italic font-bold text-lg">
+						<a href={project.link}>
+							{project.title}
+						</a>
+					</h3>
+				</article>
+				<!-- <Project {project} /> -->
+			</li>
+		{/each}
+	</ol>
+</section>
+
+<section id="open-source" class="mx-8 my-8">
+	<h2 class="font-gothic text-6xl mb-4">Open Source</h2>
+	<ul>
+		{#each data.openSource as { title, github, description }}
+			<li class="h-full">
+				<a
+					class="styled-border no-hover-style block h-full text-xs group"
+					href={github}
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<div class="flex justify-between items-center">
+						<h3 class="my-2 pointer-events-auto font-bold tracking-tighter text-lg leading-none">
+							{title}
+						</h3>
+					</div>
+					<p class="body my-1">{description}</p>
+				</a>
+			</li>
+		{/each}
+	</ul>
+</section>
+
 <!-- <section id="pinned" class="my-24">
 	<header
 		class="sm:flex justify-between items-end px-6 pb-2 pt-4 mt-16 mb-8 border-b-1 border-primary dark:border-accent"
